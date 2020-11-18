dc6422b3eed9ed1e5958532eab09b8c05967c46a
Yai <myd.rio@hotmail.com>
2020-11-18 21:40:38 +0100

    working on php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -11,6 +11,7 @@
 	</head>
 	<body>
 		<nav class="desktop-nav">
+        <?php echo "<p>Hello World!</p>";?>
 			<div class="container">
 				<div>
 					<a href="/" id="nav-logo"></a>
